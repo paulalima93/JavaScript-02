15eb04853bc9b16c63191242b3c00a1b61331376
tais-santos-dev <tais.santos.dev@gmail.com>
2026-01-31 12:08:19 -0300

    aula 14 Taís

--- app-financeiro/main.js
+++ app-financeiro/main.js
@@ -1,9 +1,29 @@
 const btnAdicionar = document.getElementById('btn-adicionar');
 const saldo = document.getElementById('texto-saldo');
+const inputDesc = document.getElementById('descricao');
+const inputValue = document.getElementById('valor');
+const listTransacoes = document.getElementById('lista-transacoes');
+
+function adicionarTransacao(){
+    let desc = inputDesc.value;
+    let value = inputValue.value;
+
+    listTransacoes.innerHTML = `
+    <li>
+        <p>${desc}</p>
+        <p>R$ ${value}</p>
+    </li>
+    `
+    saldo.innerText = value;
+    inputDesc = "";
+    inputValue = "";
+
+}
 
 function teste(){
     alert('Saldo alterado!');
     saldo.innerText = 'R$ 5.000,00'
 }
 
-btnAdicionar.addEventListener('click', teste)
+btnAdicionar.addEventListener('click', adicionarTransacao)
+
